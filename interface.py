7f3ef5550dfe37ab64416b389ea16e29245781aa
Guilherme Gabriel <guilhermegabriel.mf@gmail.com>
2025-06-27 14:54:09 -0300

    estrutura

--- interface.py
+++ interface.py
@@ -1,11 +1,11 @@
 from tkinter import *
 from tkinter import ttk, Text, Scrollbar
-from app import abrir_chrome, lancar_notas, carregar_csv
+from main import abrir_chrome, lancar_notas, carregar_csv
 
 
 root = Tk()
 frm = ttk.Frame(root, padding=10)
-frm.grid()
+frm.grid(padx=100, pady=100)
 ttk.Button(frm, text="Abrir Chrome", command=abrir_chrome).grid(column=0, row=0)
 ttk.Button(frm, text="Carregar CSV", command=carregar_csv).grid(column=0, row=1)
 ttk.Button(frm, text="Lançar notas", command=lancar_notas).grid(column=0, row=2)
